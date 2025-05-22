de0a42f9eb730c73315ab18e1ae659d656b2eee5
Alan <lufan.angel@gmail.com>
2025-05-22 22:20:37 +0800

    add note

---
+++ lua/plugins/note.lua
@@ -0,0 +1,14 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+if true then return {} end
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+--
+-- NOTE:
+--  1. 在注释行插入新的行，快速跳脱注释:在插入模式下，按 Ctrl-u 删除当前行的自动注释前缀（如果有）
+--  2.
